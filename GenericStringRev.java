1c01fb2e5bd2347c2fea09236d5ef2ebd038ec56
Tanishq Shivasharan <155131699+Tanishq747Shivasharan@users.noreply.github.com>
2025-06-27 18:37:01 +0530

    Update GenericStringRev.java

--- GenericStringRev.java
+++ GenericStringRev.java
@@ -1,35 +1,46 @@
-import java.util.*;
-
 public class GenericStringRev {
-    public static <T> void reverseList(List<T> l){
-        int left = 0;
-        int right = l.size()-1;
 
-        while(left<right) {
-            T temp =l.get(left);
-            l.set(left, l.get(right));
-            l.set(right, temp);
+    public static <T> void reverseArray(T[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            T temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             left++;
             right--;
         }
     }
 
-    public static void main(String[] args) {
+    public static Character[] toCharacterArray(String input) {
+        Character[] res = new Character[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            res[i] = input.charAt(i);
+        }
+        return res;
+    }
 
+    public static <T> String arrayToString(T[] array) {
+        StringBuilder builder = new StringBuilder();
+        for (T element : array) {
+            builder.append(element);
+        }
+        return builder.toString();
+    }
+
+    public static void exe() {
         String in = "Generics are cool";
 
-        Character[] charArray = new Character[in.length()];
-        for (int i = 0; i < in.length(); i++) {
-            charArray[i] = in.charAt(i);
-        }
+        Character[] ca = toCharacterArray(in);
+        reverseArray(ca);
+        String rev = arrayToString(ca);
 
-
-        reverseList(charList);
-        StringBuilder reversed = new StringBuilder();
-        for (char c : charList) {
-            reversed.append(c);
-        }
         System.out.println("Original String: " + in);
-        System.out.println("Reversed String: " + reversed.toString());
+        System.out.println("Reversed String: " + rev);
+    }
+
+    public static void main(String[] args) {
+        exe(); 
     }
 }
